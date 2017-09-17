2f2508daa9c34c0ca70a6815b7b4c99f1dd83f1c
phro <jesse.frohlich@ualberta.ca>
2017-09-17 15:42:31 -0400

    Add some radical functions for data analysis
      - costPer_ takes in a list of purchases and gives you the price paid
      over that time period (until the last day of purchase)
      - since takes in a day (in year month day format) and a list of
      purchases and gives you all purchases made since that date.

--- Budget.hs
+++ Budget.hs
@@ -1,17 +1,46 @@
 module Budget
   (Widget
   ,widget
-  ,totalCost
+  ,day
+  ,cost
+  ,description
+  ,timeSpan
+  ,costPerDay
+  ,costPerWeek
+  ,costPerMonth
+  ,since
   ) where
 import Data.Time.Calendar
 type Description = String
 type Cost = Float
-data Widget = Widget {getDay :: Day, getCost :: Cost, getDescription :: Description} deriving (Eq,Show)
+data Widget = Widget
+  {day :: Day
+  ,cost :: Cost
+  ,description :: Description
+  } deriving (Eq,Show)
 
 widget :: Integer -> Int -> Int -> Cost -> Description -> Widget
-widget y m d c de = Widget {getDay = (fromGregorian y m d), getCost = c, getDescription = de}
+widget y m d c de = Widget {day = (fromGregorian y m d), cost = c, description = de}
 
-totalCost :: [Widget] -> Cost
-totalCost = foldr (\w s -> getCost w + s) 0
+timeSpan :: [Widget] -> Integer
+timeSpan = ((-) <$> maximum <*> minimum) . (map (toModifiedJulianDay . day))
+
+costPerDay :: [Widget] -> Float
+costPerDay = (/) <$> (sum . map cost) <*> ((1+) . fromInteger. timeSpan)
+
+costPerWeek :: [Widget] -> Float
+costPerWeek = (7*). costPerDay
+
+costPerMonth :: [Widget] -> Float
+costPerMonth = (30.4167*). costPerDay
+
+since :: Integer -> Int -> Int -> [Widget] -> [Widget]
+since y m d = filter $ (fromGregorian y m d <=) . day
+
+
+--[costPerWeek $ filter (((fromGregorian 2017 09 n)<=).day) groceries | n <- [3..16]]
+
+--cost :: [Widget] -> Cost
+--cost = foldr (\w s -> getCost w + s) 0
 -- Insert useful budgeting functions here
 
